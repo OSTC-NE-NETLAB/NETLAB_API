9e568bb20cd70496e2f5620b655eb5842413b7c2
hopefulfire <hopefulfireus@gmail.com>
2025-06-01 02:01:15 -0400

    made new API systems

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const port = 8080
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('main.db');
 
+app.use(express.json())
 
 app.get('/', (req, res) => {
   res.send(302);
@@ -12,15 +13,20 @@ app.get('/', (req, res) => {
 
 app.get('/inventory', async (req, res) => {
   console.log("inventory DB request from " + req.id + "/inventory -> GET request");
-  await db.get("SELECT * FROM inventory", (err, row) =>{
+  await db.all("SELECT * FROM inventory", (err, row) =>{
     if (err){
       res.status(400);
       return;
     }
     res.json({
-      "data" : row,
+      row,
     })
   });
+})
+
+app.get('/login', (req, res) => {
+  console.log("login request from " + req.ip + " /login -> GET request")
+  
 })
 
 app.listen(port, () => {
