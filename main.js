98e64e09ba3aa9ca2dda3f8ed724ccfaaa4447fd
hopefulfire <hopefulfireus@gmail.com>
2025-06-01 23:09:07 -0400

    login sys start :D im sane trust

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const app = express()
 const port = 8080
 const sqlite3 = require('sqlite3').verbose();
 const db = new sqlite3.Database('main.db');
+const crypto = import('node:crypto');
+
 
 app.use(express.json())
 
@@ -24,10 +26,32 @@ app.get('/inventory', async (req, res) => {
   });
 })
 
-app.get('/login', (req, res) => {
+app.get('/login', async (req, res) => {
   console.log("login request from " + req.ip + " /login -> GET request")
   
+  let username = await req.body.username;
+  let password = await req.body.password;
+  let date = new Date();
+  let usercheck = false;
+  if (username != undefined && password != undefined){
+    db.all('SELECT username, password FROM auth', (err, row) =>{
+      if(err) {
+        res.status(400);
+      }
+      for(i=0; i <= row.length; i++){
+        if(username == row[i].username && password == row[i].password){
+          console.log("login")
+          usercheck = true;
+          res.send(200)
+          break;
+        }
+      }
+    })
+  }
+  else{res.send(400)}
 })
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
