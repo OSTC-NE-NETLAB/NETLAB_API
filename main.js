dc0d309249830c35ad0dc4538e53e38f964f5b8d
hopefulfire <hopefulfireus@gmail.com>
2025-09-04 22:21:17 -0400

    you know updated the repo

--- main.js
+++ main.js
@@ -1,50 +1,38 @@
 const express = require('express')
 const app = express()
 const port = 8080
-const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('main.db');
-const crypto = import('node:crypto');
+const sqlite = require('node:sqlite');
+const { DatabaseSync } = require('node:sqlite');
+const database = new DatabaseSync('/home/lavapuppydog/repos/NETLAB_API/main.db');
+const fs = require('fs');
 
-
+//calls express 
 app.use(express.json())
+
+//log for the request
+app.use((req, res, next) => {
+  var log = 'Request Info:' + ' Time:' + new Date() + ' Type:' + req.method + ' Ip:' + req.ip;
+  
+  fs.appendFile('logs.txt','\n' + log, (err) => {
+    if (err) throw err;
+  }); 
+ next()
+})
+
+
+
 
 app.get('/', (req, res) => {
   res.send(200);
-  console.log("status request from " + req.ip + " / -> GET request");
 })
 
-app.get('/login', async (req, res) => {
-  console.log("login request from " + req.ip + " /login -> GET request")
-  
-  let username = await req.body.username;
-  let password = await req.body.password;
-  let date = new Date();
-  let usercheck = false;
-  if (username != undefined && password != undefined){
-    db.all('SELECT username, password FROM auth', (err, row) =>{
-      if(err) {
-        res.status(400);
-      }
-      for(i=0; i <= row.length; i++){
-        try{if(username == row[i].username && password == row[i].password){
-          console.log("login")
-          usercheck = true;
-          res.send(200)
-          break;
-        }}
-        catch(err){
-          res.send(400)
-          break;
-            }
-      }
-    })
-  }
-  else{res.send(400)}
-})
-
-
+app.post('/login', (req, res) =>{
+ let username = req.body.username;
+ let password = req.body.password;
+ const user_pass = database.prepare("SELECT username, password FROM auth WHERE username='"+ username +"' AND password='"+ password +"';")
+ let user_db = user_pass.all()
+ console.log(user_db.)
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
