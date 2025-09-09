180ad3361a1a349cfef17a700499df9c870a11d2
hopefulfire <hopefulfireus@gmail.com>
2025-09-09 04:33:20 -0400

    changed stuff

--- main.js
+++ main.js
@@ -6,6 +6,31 @@ const { DatabaseSync } = require('node:sqlite');
 const database = new DatabaseSync('/home/lavapuppydog/repos/NETLAB_API/main.db');
 const fs = require('fs');
 
+//special vars
+var return_code;
+var return_body;
+var retuen_headers;
+
+//text on start 
+console.log(
+
+"███╗░░██╗███████╗████████╗██╗░░░░░░█████╗░██████╗░  ░█████╗░██████╗░██╗\n"+
+"████╗░██║██╔════╝╚══██╔══╝██║░░░░░██╔══██╗██╔══██╗  ██╔══██╗██╔══██╗██║\n"+
+"██╔██╗██║█████╗░░░░░██║░░░██║░░░░░███████║██████╦╝  ███████║██████╔╝██║\n"+
+"██║╚████║██╔══╝░░░░░██║░░░██║░░░░░██╔══██║██╔══██╗  ██╔══██║██╔═══╝░██║\n"+
+"██║░╚███║███████╗░░░██║░░░███████╗██║░░██║██████╦╝  ██║░░██║██║░░░░░██║\n"+
+"╚═╝░░╚══╝╚══════╝░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═════╝░  ╚═╝░░╚═╝╚═╝░░░░░╚═╝\n"+
+"║█████████████████████████████████████████████████████████████████████╗\n"+
+"║ ╚═══════════════════════════════════════════════════════════════════╝\n"+
+"║                         BY: SAMUEL MASKER                           ║\n"+
+"║                FOR INTERNAL USE ONLY WITH THE NE-OSTC               ║\n"+
+"║                       LAST UPDATE:  9/08/25                         ║\n"+
+"║                      LICENSED UNDER GPL 2.0+                        ║\n"+
+"╚═════════════════════════════════════════════════════════════════════╝\n"
+)
+
+
+
 //calls express 
 app.use(express.json())
 
@@ -26,16 +51,23 @@ app.get('/', (req, res) => {
   res.send(200);
 })
 
+//main login function
 app.post('/login', (req, res) =>{
  let username = req.body.username;
  let password = req.body.password;
  const user_pass = database.prepare("SELECT username, password FROM auth WHERE username='"+ username +"' AND password='"+ password +"';")
  let user_db = user_pass.all()
- if(user_db.length < 0){
-  res.send()
- } 
+ if(user_db.length == 0){
+  return_code = 406;
+ } else{
+  return_code = 202;
+ }
+ res.send(return_code);
 })
 
+//cryptography keys
+
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Currently Listening on ${port}`)
 })
