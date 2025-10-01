4b1df6c61a990ad23752ec2cac4028cfad904d07
Someone Somewhere <NOT@NOT.com>
2025-10-01 08:01:59 -0400

    rewriting this crappy code :3

--- main.js
+++ main.js
@@ -15,9 +15,6 @@ const config = toml.parse(fs.readFileSync('./server-config.toml', 'utf-8'));
 const port = config.server_interface.port;
 
 
-//Keys for encryption -- CHANGE IN CONFIG.TOML
-const PrivateKey =  fs.readFileSync(config.server_certificates.PrivateKey, "utf-8");
-const PublicKey =  fs.readFileSync(config.server_certificates.PublicKey, "utf-8");
 
 //text on start 
 console.log(
@@ -42,305 +39,58 @@ console.log(
 //Starts Express -- DO NOT MOVE 
 app.use(express.json())
 app.use(cookieParser())
+app.use(express.static(__dirname + '/public'))
 
-//sets up expresses static dir -- !UNUSED VAR! -- DO NOT REMOVE
-const options = {
-  dotfiles: 'ignore',
-  etag: false,
-  extensions: "css",
-  index: false,
-  maxAge: '0',
-  redirect: false,
-  setHeaders (res, path, stat) {
-    res.set('x-timestamp', Date.now())
-  }
-}
-
-//Allows access to the ./public folder -- DO NOT MOVE
-app.use(express.static(path.join(__dirname, "public")))
-
-
-
-//log for the request -- !may change!
-app.use((req, res, next) => {
-  var log = 'Request Info:' + ' Time:' + new Date() + ' Type:' + req.method + ' Ip:' + req.ip;
-  
-  fs.appendFile('logs.txt','\n' + log, (err) => {
-    if (err) throw err;
-  }); 
-  
- next()
-})
-
-
-
-//login route -- DO NOT MOVE BELOW THE AUTH MIDDLEWARE
-app.route('/')
-  .post(async (req, res) =>{
-
-    let username = await Buffer.from(req.body.username).toString('base64');
-    let password = req.body.password
-    if(username && password){
-
-      let user_pass = database.prepare("SELECT username, password, userid FROM auth WHERE username= ?")
-      let user_db = user_pass.all(username)
-      let autherizedPass = await bcrypt.compare(password, user_db[0].password)
-      if(user_db.length > 0 && autherizedPass){
-        var Session = await genSession(user_db[0].username, user_db[0].userid);
-        let update = database.prepare("UPDATE auth SET Session = ? WHERE username= ? AND password= ?")
-        update.run(Session.Session, user_db[0].username, user_db[0].password)
-        let getinfo = await database.prepare("SELECT userid, username, Session FROM auth WHERE Session= ?;");
-        let sessioninfo = getinfo.all(Session.Session)
-        
-        res.status(202)
-          .cookie('authorization', JSON.stringify(Session), { 
-            maxAge: 60 * 60 * 1000, 
-            httpOnly: true,         
-            secure: true,           
-            path: '/'               
-          })
-          .json(
-            {message: 'login successful', 
-            username : sessioninfo[0].username, 
-            userid : sessioninfo[0].userid}
-          )
-        
-      } else{
-        res.status(401).json({message : "Bad username or password"})
-      }
-    }else{
-      res.status(400).json({message : "Bad Request, client js error"});
-    } 
- 
-    })
-  .get((req, res) =>{
-    try{
-      res.sendFile(path.join(__dirname, '/html/login.html'))
-    }catch(err){
-      res.send(500)
-      let log = 'Request info:' + ' Time: ' + new Date() + ' Type: ' + req.method + ' Ip:' + req.ip;
-      fs.appendFile('logs.txt', '\n' + log + ' Error Thrown: ' +  err, (error)=>{
-        if (error) throw error;}
-        
-      )}
-  })
-
-//sign up page -- DO NOT MOVE BELOW THE AUTH MIDDLEWARE
-app.route('/signup')
-  .get((req, res) => {
-    res.sendFile(path.join(__dirname + '/html/signup.html'))
-  })
-  .post(async (req, res) => {
-      let firstName= req.body.firstName;
-      let lastName= req.body.lastName;
-      let password = req.body.password;
-      let username = req.body.username;
-      if(firstName && lastName && password && username){
-        let Available = await checkAvailable(username);
-        if(Available){  
-        let id = await storeNewAuth(username, password)
-        storeNewUser(firstName, lastName, id)
-        res.status(202).json({message : "user successfully created"})
-        }else{
-          res.status(409).json({message : "username already in use"})
-        }
-      }else{
-        res.status(400).json({message : 'Malformed Request'})
-      }
-  })
-
-//check auth middleware -- ANYTHING UNDER THIS MUST GIVE AUTH HEADERS
-app.use( async (req,res, next) => {
-  if(req.cookies.authorization){
-    let sessioninf = await JSON.parse(req.cookies.authorization);
-    try{var session = sessioninf.Session;
-    var auth = sessioninf.sig;}
-    catch(err){
-      res.status(401);
-    }
-  }else{
-    res.send(401).redirect('/')
-  }
-  if(session && auth){
-    let authed = await verifySession(session, auth)
-    if(authed){
-      next()
-    }else{
-      res.status(401).redirect('/')
-    }
-  }else{
-    res.status(400).redirect('/');
-    
-  }
-})
-
-
-//homepage
-app.route('/menu')
-  .get(async (req, res)=>{
-    res.sendFile(path.join(__dirname + "/html/menu.html"));
-  })
-
-app.route('/main')
-  .get(async (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/main.html"))
-  })
-
-app.route('/inventory')
-  .get(async (req, res) => {
-    res.sendFile(path.join(__dirname + "/html/inventory.html"))
-  })
-
-app.route('/inventory/items/')
-  .get(async (req, res) =>{
-      let getItems = database.prepare("SELECT asset_id, category, description, date FROM inventory")
-      let Items = getItems.all()
-      res.send(Items)
-  })
-app.route('/inventory/update')
-  .post(async (req, res) =>{
-    let asset = req.body.asset_id;
-    let category = req.body.category;
-    let description = req.body.description;
-    let date = req.body.date;
-    if(asset && category && description && date){
-      try{
-         let accepted = insDatabase(asset, category, description, date)
-         res.status(200).json({status : accepted})
-      }catch(err){
-        res.sendStatus(500).end();
-        throw err;
-      }
-    }else{
-      res.status(400).json({message : "Malformed Request"})
-    }
-  })
-
-/*#####################################################################################
-FUNCTION AREA 
-#####################################################################################*/
-//input data into inventory
-async function insDatabase(asset, category, description, date){
-  try{
-    let inData = database.prepare('INSERT INTO inventory(asset_id, category, description, date) VALUES (?,?,?,?)')
-    inData.run(asset, category, description, date);
-    return true;}
-  catch(err){
-    return false;
-  }
-}
-
-//session key generator
-async function genSession (username, userid){
-  let data = {
-    username : username,
-    userid : userid,
-    exp: Math.floor(Date.now() / 1000) + (60 * 60),
-    init: Math.floor(Date.now()),
-  }
-  let payload = JSON.stringify(data)
-  let Session = crypto.publicEncrypt(
-  {
-    key: PublicKey,
-    padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-    oaepHash: "sha256",
-  },
-
-  Buffer.from(payload));
-  let SessionSig =  crypto.sign("sha256", Buffer.from(Session), {
-  key: PrivateKey,
-  padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
-  });
-  let SignedEncryp = {
-    sig : SessionSig.toString('base64'),
-    Session: Session.toString('base64'),
-  }
-  return SignedEncryp;
-}
-
-//session expiration and authentication 
-async function verifySession(data, sig){
-// Verify signature over the encrypted session
-  let decryptedBuf = await crypto.privateDecrypt(
-    {
-      key: PrivateKey,
-      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-      oaepHash: 'sha256',
-    },
-    Buffer.from(data, 'base64')
-  );
-  let datajson = JSON.parse(decryptedBuf)
-  let expire = datajson.exp;
-  if(expire >= Math.floor(Date.now() / 1000)){
-    var isValid = crypto.verify(
-      'sha256',
-      Buffer.from(data, 'base64'),
-      {
-        key: PublicKey,
-        padding: crypto.constants.RSA_PKCS1_PSS_PADDING,
-      },
-      Buffer.from(sig, 'base64')
-    );
-      if(isValid){
-        let getUser = database.prepare('SELECT username FROM auth WHERE Session = ?')
-        let checkStat = getUser.all(data);
-        if(checkStat.length >= 1){return true}
-      }else{
-        return false
-      }
-  }
-  else{
-    return false
-  }
-}
-async function checkAvailable(username) {
-  let user64 = await Buffer.from(username).toString('base64')
-  let check = database.prepare("SELECT userid FROM auth WHERE username = ?")
-  let Available = check.all(user64)
-  if(Available.length != 0){
-    return false
-  }else{
-    return true;
-  }
-}
-
-//store the username & password into the auth table
-async function storeNewAuth(username, password) {
-  let username64 = await Buffer.from(username).toString('base64')
-  let passwordHash = await bcrypt.hash(password, 12)
-  try{let insertUser = database.prepare("INSERT INTO auth(username, password) VALUES (?, ?)")
-  insertUser.run(username64, passwordHash)
-  let getid = database.prepare("SELECT userid FROM auth WHERE username = ? ")
-  let id = getid.all(username64)
-  return id[0].userid;
-  }
-  catch(err){
-    throw err
-  }
-}
-
-//store the first & last name aswell as the id and date made in users
-async function storeNewUser(first, last, id) {
-  let time = new Date().toUTCString();
-  let first64 = await Buffer.from(first).toString('base64')
-  let last64 = await Buffer.from(last).toString('base64')
-  try{
-    let insertId = database.prepare('INSERT INTO users(userid) VALUES (?)')
-    await insertId.run(id)
-    let insertUser = database.prepare("UPDATE users SET first = ?, last = ?, made = ? WHERE userid = ?")
-    await insertUser.run(first64, last64, time, id)
-    return true
-  }catch(err){
-    throw err;
-  } 
-}
-/**#######################################################################################
- * END OF FUNCTION AREA
- * #######################################################################################
+/**##################################################################################################################
+ * 
+ * FUNCTIONS
+ * 
+ * ##################################################################################################################
  */
 
+async function AuthenticateUser(username, password) {
+  database.prepare("SELECT userid, username, password, FROM auth WHERE username=")
+}
 
 
-app.listen(port, () => {
-  console.log(`Currently Listening on ${port}`)
+
+
+/**###################################################################################################################
+ * 
+ * ROUTES && MIDDLEWARE
+ * 
+ * ####################################################################################################################
+ */
+
+app.route('/login')
+    .get((req, res) => {
+      res.status(200).sendFile(path.join(__dirname + '/html/login.html'))
+    })
+
+
+app.route('/signup')
+    .get((req, res) => {
+      res.status(200).sendFile(path.join(__dirname + '/html/signup.html'))
+    })
+
+
+// middleware for signin
+app.use((req, res, next) =>{
+  let auth  = req.cookies.autherization;
+  if(auth){
+    res.send(200)
+  }else{
+    res.status(401).redirect('/login')
+  }
+})
+
+
+app.route('/')
+  .get((req, res)=> {
+    console.log('hello world')
+  })
+
+
+app.listen(port, () =>{
+  console.log(`NETLAB API IS LISTEN ON ${port}`)
 })
--- public/client.js
+++ public/client.js
@@ -1,179 +1,20 @@
-const { response } = require("express");
 
-function goToSignup(){
-        window.location.href = window.location.origin + '/signup';
-}
-function goToLogin(){
-        window.location.href = window.location.origin + '/login';
-}
-
-
-async function attemptLogin(){
-        let usernamein = document.getElementById('username').value;
-        let passwordin = document.getElementById('password').value;
-        var failed = false;
-        var getdata;
-        let send = {
-            username : usernamein,
-            password : passwordin
-        }
-        try {var getdata = await fetch(window.location.href, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-         },
-         body: JSON.stringify(send)
-        })
-        }catch(err) {throw err;}
-        if(await getdata.status != 202){
-                failed = true
-        }
-
-        if(!failed){
-                let data = await getdata.json();
-                try{
-                        delete data.message;
-                        let userdata = {
-                                userid : data.userid,
-                                username : data.username
-                        }
-                        localStorage.setItem('userdata', JSON.stringify(userdata))
-                }catch(err){throw err}
-                goHome()
-        }else{
-                console.log("bad response")
-        }
-        
-    }
-
-async function goHome(){
-        let post;
-        try {post = await fetch(window.location.origin + "/menu", {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'document/html'
-         }
-        })
-        }catch(err) {throw err;}
-        if(post.status == 401){
-                return 401;
-        }else{  
-                
-                document.open();
-                document.write(post.text());
-                document.close();
-                return 202;
-        }
-    }
-
-async function getMainContent() {
-        let post;
-        try {post = await fetch(window.location.origin + "/main", {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'document/html'
-         }
-        })
-        }catch(err) {throw err;}
-
-
-        document.getElementById('content_window').setAttribute('srcdoc', post.text())
-        
-        
-}
-
-function signOut(){
-        localStorage.removeItem('session');
-        localStorage.removeItem('userdata');
-        window.location.reload();
-}
-async function signUp(){
-        let username = document.getElementById('username').value;
-        let password = document.getElementById('password').value;
-        let firstName = document.getElementById('first').value;
-        let lastName = document.getElementById('last').value;
-        let code;
-        let data = {
-                username : username,
-                password : password,
-                firstName : firstName,
-                lastName : lastName,
-        }
-
-        try {await fetch(window.location.origin + "/signup", {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-         },
-        body: JSON.stringify(data)
-        })
-        .then(response => code = response.status())
-        }catch(err) {throw err;}
-        console.log(code)
-}
-
-
-async function getInv(){
-        try {await fetch(window.location.origin + "/inventory", {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'document/html'
-         }
-        })
-        .then(response => response.text())
-        .then(content => {
-        document.getElementById('content_window').setAttribute('srcdoc', content)})
-        }catch(err) {throw err;}
-}
-async function putInv(){
-        let asset = document.getElementById('asset_id').value;
-        let category = document.getElementById('category').value;
-        let description = document.getElementById('description').value;
-        let date = document.getElementById('date').value;
-        let data = {
-            asset_id : asset,
-            category : category,
-            description: description,
-            date : date
-        }
-        console.log(window.location)
-        try {await fetch(window.location.hostname + "/inventory/update", {
-        method: 'POST',
-        body : JSON.stringify(data),
-        })
-        }catch(err) {throw err;}
-        location.reload()
-}
-async function getItems(){
-        let jsondata;
-        try {await fetch(window.location.hostname + "/inventory/items", {
-        method: 'GET',
-        headers: {
-          'Content-Type': 'application/json'
-         }
-        })
-        .then(response=>response.json())
-        .then(data=> jsondata = data)
-        }catch(err) {throw err;}
-        
-        for (let i = 0; i < jsondata.length; i++){
-                var tr = document.createElement('tr')
-                var asset = document.createElement('td')
-                var category = document.createElement('td')
-                var description = document.createElement('td')
-                var date = document.createElement('td')
-                tr.setAttribute('id', `item${i}`)
-
-                asset.innerHTML = jsondata[i].asset_id;
-                category.innerHTML = jsondata[i].category;
-                description.innerHTML = jsondata[i].description;
-                date.innerHTML = jsondata[i].date;
-
-                tr.appendChild(asset)
-                tr.appendChild(category)
-                tr.appendChild(description)
-                tr.appendChild(date)
-
-                document.getElementById('InvBody').appendChild(tr)
-        }
+async function signin() {
+   let username =  document.getElementById('username').value
+   let password = document.getElementById('password').value;
+   let data = {
+    username : username,
+    password : password,
+   }
+   try{
+    let response = await fetch('/login', {
+        method : 'POST',
+        body: data,
+    })
+   }catch(err){
+    throw err
+   }
+   if(response.status == 201){
+    document.location.href = document.location.href + '/'
+   }
 }
